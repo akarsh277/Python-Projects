85ac26feac3c90273cb29cf18f373cedebd327ed
AKARSH VIJJAPU <akarshvijjapu@gmail.com>
2026-09-17 14:10:22 +0530

    Add files via upload

---
+++ Project_3_Student_Management_API/data.py
@@ -0,0 +1,37 @@
+students = [
+    {
+        "id": 1,
+        "name": "Akarsh",
+        "age": 20,
+        "department": "CSE",
+        "email": "akarsh@gmail.com",
+    },
+    {
+        "id": 2,
+        "name": "Rahul",
+        "age": 21,
+        "department": "ECE",
+        "email": "rahul@gmail.com",
+    },
+    {
+        "id": 3,
+        "name": "Priya",
+        "age": 19,
+        "department": "IT",
+        "email": "priya@gmail.com",
+    },
+    {
+        "id": 4,
+        "name": "Arjun",
+        "age": 22,
+        "department": "ME",
+        "email": "arjun@gmail.com",
+    },
+    {
+        "id": 5,
+        "name": "Sneha",
+        "age": 20,
+        "department": "EEE",
+        "email": "sneha@gmail.com",
+    },
+]
